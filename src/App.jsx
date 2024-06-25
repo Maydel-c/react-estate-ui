5240a11abbc2873ef127cce4054ef532547c9400
Abhinav <vanihba.arora@gmail.com>
2024-06-25 09:25:10 +0530

    complete UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,50 @@
-import Navbar from './components/navbar/Navbar'
-import './layout.scss'
 import HomePage from './pages/homePage/homePage'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import ListPage from './pages/listPage/listPage'
+import Layout from './pages/layout/layout'
+import SinglePage from './pages/singlePage/singlePage'
+import Login from './pages/login/login'
+import ProfilePage from './pages/profilePage/profilePage'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element:<HomePage />
+        },
+        {
+          path: "/list",
+          element:<ListPage />
+        },
+        {
+          path: "/:id",
+          element:<SinglePage />
+        },
+        {
+          path: "/login",
+          element:<Login />
+        },
+        {
+          path: "/profile",
+          element:<ProfilePage />
+        }
+      ]
+    }    
+  ]) 
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
+
+    
+
+
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/components/map/map.jsx
@@ -0,0 +1,23 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import './map.scss';
+import 'leaflet/dist/leaflet.css'
+import Pin from '../pin/pin';
+
+const Map = ({items}) => {
+    const position = [51.110, -0.09]
+    return (        
+  <MapContainer center={position} zoom={7} scrollWheelZoom={false} className='map'>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    
+    {items.map((item) => (
+        <Pin item={item} key={item.id} />
+    ))}        
+    
+    </MapContainer>
+    )
+}
+ 
+export default Map;
